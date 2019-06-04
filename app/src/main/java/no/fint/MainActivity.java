95346d3b833ded3177ff46140330f8378473e54c
Ola <olastaalberg@gmail.com>
2019-06-04 10:29:42 +0200

    endret litt på gui-colors. Prøver å få til addOnMenuItemClickListener, men er feil nå

--- app/src/main/java/no/fint/MainActivity.java
+++ app/src/main/java/no/fint/MainActivity.java
@@ -3,11 +3,17 @@ package no.fint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import io.github.kobakei.materialfabspeeddial.FabSpeedDial;
@@ -31,5 +37,27 @@ public class MainActivity extends AppCompatActivity {
 
 
         final FabSpeedDial fabSpeedDialMeny = findViewById(R.id.fab_menu);
+
+        FloatingActionButton miniFab0 = fabSpeedDialMeny.getMiniFab(0);
+        FloatingActionButton miniFab1 = fabSpeedDialMeny.getMiniFab(1);
+        FloatingActionButton miniFab2 = fabSpeedDialMeny.getMiniFab(2);
+        FloatingActionButton miniFab3 = fabSpeedDialMeny.getMiniFab(3);
+        final int id = miniFab0.getId();
+
+        fabSpeedDialMeny.addOnMenuItemClickListener(new FabSpeedDial.OnMenuItemClickListener() {
+            @Override
+            public void onMenuItemClick(FloatingActionButton miniFab, @Nullable TextView label, int itemId) {
+                System.out.println(itemId);
+                System.out.println(id);
+                if (itemId == id){
+                    BussKortFragment bussKortFragment = BussKortFragment.newInstance();
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager
+                            .beginTransaction();
+                    fragmentTransaction.add(R.id.fragment_container,
+                            bussKortFragment).addToBackStack(null).commit();
+                }
+            }
+        });
     }
 }
