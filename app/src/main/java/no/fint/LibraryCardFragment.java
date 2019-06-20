b89dabaecd959c5f1d2c7ae25742de0bf9c161b5
Ola <olastaalberg@gmail.com>
2019-06-20 09:50:59 +0200

    cleaning up more

--- app/src/main/java/no/fint/LibraryCardFragment.java
+++ app/src/main/java/no/fint/LibraryCardFragment.java
@@ -12,12 +12,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class LibraryCardFragment extends Fragment implements View.OnClickListener {
-    LinearLayout linearLayoutTop;
     LinearLayout linearLayoutBottom;
     private OnFragmentInteractionListener mListener;
 
     public LibraryCardFragment() {
-        // Required empty public constructor
     }
 
     public static LibraryCardFragment newInstance() {
@@ -34,24 +32,14 @@ public class LibraryCardFragment extends Fragment implements View.OnClickListene
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_library_card, container, false);
-        //linearLayoutTop = view.findViewById(R.id.library_card_fragment_top);
         linearLayoutBottom = view.findViewById(R.id.library_card_fragment_bottom);
         linearLayoutBottom.setOnClickListener(this);
-        //linearLayoutTop.setOnClickListener(this);
-
         TextView verifiedBusCardText = view.findViewById(R.id.library_card_verified_text);
         AnimatorSet glow = (AnimatorSet) AnimatorInflater.loadAnimator(view.getContext(), R.animator.verified_animation);
         glow.setTarget(verifiedBusCardText);
         glow.start();
 
         return view;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
@@ -67,16 +55,6 @@ public class LibraryCardFragment extends Fragment implements View.OnClickListene
         }
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
