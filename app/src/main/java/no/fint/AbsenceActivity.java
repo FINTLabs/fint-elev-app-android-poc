7771e329d9847438ed1cf1013f94f24312ed9e8d
Ola <olastaalberg@gmail.com>
2019-06-20 15:37:21 +0200

    added numberpickers for absence

--- app/src/main/java/no/fint/AbsenceActivity.java
+++ app/src/main/java/no/fint/AbsenceActivity.java
@@ -1,6 +1,9 @@
 package no.fint;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,13 +12,18 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+
+import java.lang.reflect.Field;
 
 public class AbsenceActivity extends AppCompatActivity {
 
@@ -64,6 +72,47 @@ public class AbsenceActivity extends AppCompatActivity {
                 .beginTransaction();
         fragmentTransaction.add(R.id.absence_fragment_container,
                 fragment).addToBackStack(null).commit();
+    }
+
+    public static void setNumberPickerColor(NumberPicker numberPicker, int color) {
+
+        try {
+            Field selectorWheelPaintField = numberPicker.getClass()
+                    .getDeclaredField("mSelectorWheelPaint");
+            selectorWheelPaintField.setAccessible(true);
+            ((Paint) selectorWheelPaintField.get(numberPicker)).setColor(color);
+        } catch (NoSuchFieldException e) {
+            Log.w("numberPickerTextColor", e);
+        } catch (IllegalAccessException e) {
+            Log.w("numberPickerTextColor", e);
+        } catch (IllegalArgumentException e) {
+            Log.w("numberPickerTextColor", e);
+        }
+
+        final int count = numberPicker.getChildCount();
+        for (int i = 0; i < count; i++) {
+            View child = numberPicker.getChildAt(i);
+            if (child instanceof EditText)
+                ((EditText) child).setTextColor(color);
+        }
+        Field[] pickerFields = NumberPicker.class.getDeclaredFields();
+        for (Field pf : pickerFields) {
+            if (pf.getName().equals("mSelectionDivider")) {
+                pf.setAccessible(true);
+                try {
+                    ColorDrawable colorDrawable = new ColorDrawable(color);
+                    pf.set(numberPicker, colorDrawable);
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                } catch (Resources.NotFoundException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+        numberPicker.invalidate();
     }
 
 }
--- app/src/main/java/no/fint/AddAbsenceFragment.java
+++ app/src/main/java/no/fint/AddAbsenceFragment.java
@@ -1,14 +1,17 @@
 package no.fint;
 
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 
 public class AddAbsenceFragment extends Fragment implements View.OnClickListener {
     private OnFragmentInteractionListener mListener;
+    private NumberPicker absenceDayPicker, absenceHourPicker;
 
     public AddAbsenceFragment() {
     }
@@ -27,6 +30,18 @@ public class AddAbsenceFragment extends Fragment implements View.OnClickListener
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_absence, container, false);
+        absenceDayPicker = view.findViewById(R.id.number_picker_absence_days);
+        absenceHourPicker = view.findViewById(R.id.number_picker_absence_hours);
+        AbsenceActivity.setNumberPickerColor(absenceDayPicker, Color.WHITE);
+        AbsenceActivity.setNumberPickerColor(absenceHourPicker, Color.WHITE);
+        absenceDayPicker.setMaxValue(5);
+        absenceDayPicker.setMinValue(0);
+        absenceDayPicker.setDisplayedValues(new String[]{"0 dager ","1 dager", "2 dager", "3 dager", "4 dager", "5 dager"});
+        absenceDayPicker.setValue(1);
+        absenceHourPicker.setMaxValue(5);
+        absenceHourPicker.setMinValue(0);
+        absenceHourPicker.setDisplayedValues(new String[]{"0 timer ","1 time", "2 timer", "3 timer", "4 timer", "5 timer"});
+        absenceHourPicker.setValue(1);
         return view;
     }
 
